ecb7c46821da9aa8e47b7b2a0d7396d45966a716
sunyuting <yu.ting.sun@siemens-energy.com>
2026-04-15 14:55:42 +0800

    fix:proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 8175,
     proxy: {
       '/api': 'http://localhost:8001',
